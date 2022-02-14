4450bd1020af01f7b3406df54874686badfb8539
45NGC <nicolasnerfo@gmail.com>
2022-02-15 00:26:48 +0100

    Mod principal_window
    -Comments of features that should be added to principal_window
    
    -Added The Godfather to the JSON

--- windows/principal_window.py
+++ windows/principal_window.py
@@ -15,17 +15,20 @@ class PrincipalWindow(Gtk.Window):
 		self.set_default_size(900, 900)
 		self.set_position(Gtk.WindowPosition.CENTER)
 
-        # WINDOW ELEMENTS
-		
-		#vbox = Gtk.VBox(False, 2)
-		#vbox.pack_start(False, False, 0)
-
+		# HEADER
 		header = Gtk.HeaderBar(title="Films")
 		header.set_subtitle("2021 Catalog")
 		header.props.show_close_button = True
-
 		self.set_titlebar(header)
 
+        # WINDOW ELEMENTS
+
+		# SEARCH BAR
+		search_bar = Gtk.SearchBar()
+
+		# BUTTONS
+
+		# SCROLLED
 		scrolled = Gtk.ScrolledWindow()
 		scrolled.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
 		scrolled.add(self.flowbox)
@@ -36,7 +39,29 @@ class PrincipalWindow(Gtk.Window):
 			self.flowbox.add(cell)
 
 		# ADD ELEMENTS TO THE WINDOW
+
+		# This box will display a search bar and 3 buttons : 
+		# - Delete button 
+		# - Add button
+		# - Filter button
+		box_tools = Gtk.Box(orientation = Gtk.Orientation.HORIZONTAL)
+		box_tools.pack_start(search_bar, True, True, 0)
+		#box_tools.pack_end(delete_button, True, True, 0)
+		#box_tools.pack_end(add_button, True, True, 0)
+		#box_tools.pack_end(filter_button, True, True, 0)
+
+		# This box will display 4 buttons : 
+		# - Films 
+		# - Series 
+		# - Music 
+		# - Books
+		box_sector = Gtk.Box(orientation = Gtk.Orientation.HORIZONTAL)
+		#box_tools.pack_start(films_button, True, True, 0)
+		#box_tools.pack_start(series_button, True, True, 0)
+		#box_tools.pack_start(books_button, True, True, 0)
+		#box_tools.pack_start(music_button, True, True, 0)
+
 		box_principal = Gtk.Box(orientation = Gtk.Orientation.VERTICAL)
-		#box_principal.pack_start(vbox, False, False, 0)
+		box_principal.pack_start(box_tools, True, True, 0)
 		box_principal.pack_start(scrolled, True, True, 0)
 		self.add(box_principal)
